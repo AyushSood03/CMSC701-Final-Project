5154633b80456fcad7c5392964bdfb8e10477a1b
Ayush Sood <ayushsood03@gmail.com>
2024-05-07 23:11:17 -0400

    update bash script

--- src/build_tests.sh
+++ src/build_tests.sh
@@ -11,3 +11,19 @@ do
     W="${WINT}"
     java -cp "." buildminimizersa "../test_data/test_input/salmonella_sub.fa" ${K} "../bin/salmonella_sub_64_minimizer_${KMERWIDTH}_${W}" ${MINIMIZERSCHEME} ${KMERWIDTH} ${W}
 done
+
+for n in {2..18};
+do
+    KMERWIDTH="${n}"
+    WINT="$((20-n))"
+    W="${WINT}"
+    java -cp "." buildminimizersa "../test_data/test_input/salmonella_sub.fa" ${K} "../bin/salmonella_sub_64_minimizer_${KMERWIDTH}_${W}" ${MINIMIZERSCHEME} ${KMERWIDTH} ${W}
+done
+
+for n in {2..28};
+do
+    KMERWIDTH="${n}"
+    WINT="$((30-n))"
+    W="${WINT}"
+    java -cp "." buildminimizersa "../test_data/test_input/salmonella_sub.fa" ${K} "../bin/salmonella_sub_64_minimizer_${KMERWIDTH}_${W}" ${MINIMIZERSCHEME} ${KMERWIDTH} ${W}
+done
